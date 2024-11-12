e3fb3b430c7b45f57732257ca7c1b2688d7c3142
JuanPF56 <juanpablofresia@gmail.com>
2024-11-12 01:24:43 -0300

    Fix warnings on firebase initialization

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,4 +1,4 @@
-import { FirebaseApp, initializeApp } from '@react-native-firebase/app';
+import { FirebaseApp, getApp, getApps, initializeApp } from '@react-native-firebase/app';
 import firestore from '@react-native-firebase/firestore';
 import messaging from '@react-native-firebase/messaging';
 
@@ -9,10 +9,11 @@ const firebaseConfig = {
     storageBucket: "twitsnap-aff6b.appspot.com",
     messagingSenderId: "216459666015",
     appId: "1:216459666015:web:76945ffe345fef15fdb1ea",
-    measurementId: "G-VZ8L15XKQ4"
+    measurementId: "G-VZ8L15XKQ4",
+    databaseURL: '',
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
 const fireDB = firestore();
 const messagingInstance = messaging();
